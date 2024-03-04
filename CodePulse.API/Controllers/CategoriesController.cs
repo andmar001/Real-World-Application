8b64a9ff25150ee95104de44c21f327f1eb1c039
andmar001 <andrademaresantonio00@gmail.com>
2024-03-03 21:32:34 -0600

    DELETE weather controller

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        
+    }
+}
