419051215d366384251d60db433a30fea7763742
andmar001 <andrademaresantonio00@gmail.com>
2024-03-03 22:23:59 -0600

    ADD post categories

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
 {
@@ -6,6 +9,32 @@ namespace CodePulse.API.Controllers
     [Route("api/[controller]")]
     public class CategoriesController : Controller
     {
-        
+        private readonly ApplicationDbContext dbContext;
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            //Map DTO to Domain Model
+            Category category = new Category
+            {
+                Name = request.Name,
+                UrlHandler = request.UrlHandler
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            //Domain model to DTO
+            CategoryDTO response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandler = category.UrlHandler
+            };
+            return Ok(response);
+        }
     }
 }
